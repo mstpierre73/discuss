560cfdfa548dc47630d59623e3961176b8a2597e
Marise St-Pierre <marise.stpierre@nventive.com>
2023-01-04 16:53:41 -0500

    update topic controller for create route

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,6 +2,7 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topic
+  alias Discuss.Repo
 
   def new(conn, params) do
     struct = %Topic{}
@@ -14,7 +15,11 @@ defmodule DiscussWeb.TopicController do
   def create(conn, %{"topic" => topic} = params) do
     # IO.inspect(conn)
     # IO.inspect(params)
-
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post -> IO.inspect(post)}
+      {:error, changeset} -> IO.inspect(changeset)
+    end
   end
 
 end
